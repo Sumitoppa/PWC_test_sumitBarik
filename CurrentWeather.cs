fbf075df9c29397a14c97bc929f1c3d29c36c21a
Sumitoppa <sumitbarik944@gmail.com>
2023-03-14 14:07:21 +0530

    Add files via upload

---
+++ CurrentWeather.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumit.Model
+{
+    class CurrentWeather
+    {
+        public double Temperature { get; set; }
+        public double WindSpeed { get; set; }
+        public double WindDirection { get; set; }
+        public int WeatherCode { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ GeoLocationData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumit.Model
+{
+    class GeoLocationData
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double GenerationTime_MS { get; set; }
+        public int UTC_Offset_Seconds { get; set; }
+        public string TimeZone { get; set; }
+        public string TimeZone_Abbreviation { get; set; }
+        public double Elevation { get; set; }
+        public CurrentWeather Current_Weather { get; set; }
+    }
+}
